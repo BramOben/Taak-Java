6f679fbfa64a0b8532df63cc4f0cc116c79278cd
shitprogrammer <82523549+shiitprogrammer@users.noreply.github.com>
2023-12-19 20:26:51 +0100

    Delete Pong directory

--- Pong/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module be.inf1.tellerapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens be.inf1.tellerapp to javafx.fxml;
-    exports be.inf1.tellerapp;
-}
